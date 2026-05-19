11c5bd6001bd55dd44b4ff782f153b0a410cdfed
Olexander930 <ivancenkoninp@gmail.com>
2026-05-19 18:42:11 +0300

    Implemented API routes

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// TODO: Імпортувати контролери
 
-// A1 - Перегляд категорій
+// Імпортуємо контролер категорій
+// Саме в ньому знаходиться вся бізнес-логіка, що робити з запитом, які дані брати з БД, як формувати відповідь і т.д.
+const categoriesController = require('../controllers/categories');
+
+// Імпортуємо asyncHandler нашу обгортку для асинхронних функцій вона автоматично ловить помилки і передає їх у next(),
+const asyncHandler = require('../middleware/asyncHandler');
+
+// Перегляд категорій
 // GET /api/categories
+router.get('/', asyncHandler(categoriesController.getAll));
 
+// Експортуємо роутер назовні.
 module.exports = router;
--- src/routes/organizations.js
+++ src/routes/organizations.js
@@ -1,17 +1,47 @@
 const express = require('express');
+
+// Створюємо об'єкт роутера для маршрутів, пов'язаних з організаціями
 const router = express.Router();
-// TODO: Імпортувати контролери
 
-// A1 - Перегляд каталогу
-// GET /api/organizations?category_id=...
+// Імпортуємо контролер організацій.
+// У ньому будуть функції getAll, getById, create, importCSV, updateStatus.
+// Кожна функція відповідає за свій ендпоінт.
+const organizationsController = require('../controllers/organizations');
+
+// Імпортуємо asyncHandler 
+const asyncHandler = require('../middleware/asyncHandler');
+
+// Перегляд каталогів організацій з можливістю фільтрації за категорією та статусом
+// Логіка фільтрації буде всередині organizationsController.getAll — 
+// він прочитає req.query і передасть потрібні параметри в репозиторій
+router.get('/', asyncHandler(organizationsController.getAll));
+
 // GET /api/organizations/:id
+// Логіка отримати повну інформацію про одну конкретну організацію
+// :id — це динамічний параметр, який буде доступний у контролері як req.params.id
+// Можливо, це вважливо знати, цей маршрут має бути після GET /, бо інакше Express сприйме слово import в URL як :id.
+router.get('/:id', asyncHandler(organizationsController.getById));
 
-// A2 - Додавання організації
+// Додавання організації 
 // POST /api/organizations
+// Клієнт надсилає JSON з даними організації в тілі запиту
+// Контролер створить організацію зі статусом pending простими словами очікує модерації, і 
+// поверне її дані у відповіді.
+router.post('/', asyncHandler(organizationsController.create));
 // POST /api/organizations/import
+// Завантаження CSV-файлу з даними організацій
+// Клієнт надсилає файл через multipart/form-data з полем "file"
+// Контролер прийме файл, передасть його в csvParser,
+// провалідує кожен рядок і створить заявки зі статусом pending
+router.post('/import', asyncHandler(organizationsController.importCSV));
 
-// A3 - Модерація організацій
-// GET /api/organizations?status=pending
-// PUT /api/organizations/:id/state
+//Модерація організацій
+// PUT /api/organizations/:id/status
+// Змінити статус організації тобто можна схвалити, відхилити або архівувати заявку на додавання організації
+// ID організації, статус якої змінюємо передається в URL як :id, а новий статус в тілі запиту.
+// У тілі запиту rea.body має бути JSON з новим статусом і, якщо статус "rejected", 
+// то також з полем rejection_reason з поясненням причини відхилення
+router.put('/:id/status', asyncHandler(organizationsController.updateStatus));
 
+// Експортуємо роутер, щоб його можна було підключити в index.js
 module.exports = router;
