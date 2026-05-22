dddc86bd83131bffd31b75356379fedb21abd1d5
Enilond22 <romankuceravenko922@gmail.com>
2026-05-22 23:00:53 +0300

    chore: remove moderation routes

--- src/routes/moderationRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import * as moderationController from '../controllers/moderationController.js';
-// router.get('/pending', moderationController.getPendingOrganizations);
-// router.patch('/:id/status', moderationController.updateOrganizationStatus);
-
-export default router;
